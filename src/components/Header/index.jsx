f82624f90188c250499baed8f3d88366ba34b210
Francisco das Chagas <f.chagasdutra18@gmail.com>
2025-06-02 11:59:45 -0300

    produzindo o header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,20 @@
 const Header = () => {
-  return ( 
+  return (
     <header className="">
+      <div className="top-strip py-2">
+        <div className="container">
+            <div className="flex items-center justify-between">
+              <div className="col1 w-[50%]">
+                <p className="text-[14px] font-[500]">Get up to 50% off new season styles, limited time only</p>
+              </div>
 
+                <div className="flex"></div>
 
+            </div>
+        </div>
+      </div>
     </header>
-   );
-}
- 
+  );
+};
+
 export default Header;
