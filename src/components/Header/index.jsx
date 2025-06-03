048393217f289ee9acbb227472a6024836e2fbcc
Francisco das Chagas <f.chagasdutra18@gmail.com>
2025-06-03 18:02:04 -0300

    adicionando area de icones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,24 @@
 import { Link } from "react-router-dom";
 import Search from "../Search";
-import { Button } from "@mui/material";
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoIosGitCompare } from "react-icons/io";
+import { FaRegHeart } from "react-icons/fa";
+import Tooltip from '@mui/material/Tooltip'
+
+
+
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 const Header = () => {
   return (
@@ -40,9 +58,38 @@ const Header = () => {
           </div>
 
           <div className="col1 w-[30%] flex items-center pl-7">
-                <ul className="flex items-center gap-3">
+                <ul className="flex items-center gap-3 justify-end w-full">
                     <li className="list-none">
-                      <Link to="/login" className="link transition text-[15px] font-[500]">Login</Link> | <Link to="/register" className="link transition text-[15px] font-[500]">Register</Link>
+                      <Link to="/login" className="link transition text-[15px] font-[500]
+                      ">Login</Link> |
+                      <Link to="/register" className="link transition text-[15px] font-[500] pl-1
+                      ">Register</Link>
+                    </li>
+
+                    <li>
+                    <Tooltip title="compare">
+                      <IconButton aria-label="cart">
+                        <StyledBadge badgeContent={4} color="secondary">
+                        <IoIosGitCompare />
+                        </StyledBadge>
+                      </IconButton>
+                    </Tooltip>
+                    </li>
+
+                    <li>
+                    <IconButton aria-label="cart">
+                      <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart />
+                      </StyledBadge>
+                    </IconButton>
+                    </li>
+
+                    <li>
+                    <IconButton aria-label="cart">
+                      <StyledBadge badgeContent={4} color="secondary">
+                      <MdOutlineShoppingCart />
+                      </StyledBadge>
+                    </IconButton>
                     </li>
                 </ul>
           </div>
