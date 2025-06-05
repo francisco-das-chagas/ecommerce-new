2b7e61a5dac893ca22a1de0972dff9592b47e561
Francisco das Chagas <f.chagasdutra18@gmail.com>
2025-06-05 15:31:07 -0300

    implementação de botões de navegação

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -5,12 +5,11 @@ import { LiaAngleDownSolid } from "react-icons/lia";
 import { Link } from "react-router-dom";
 import { GoRocket } from "react-icons/go";
 
-
 const Navigation = () => {
   return (
     <nav className="py-2">
       <div className="container flex items-center justify-end gap-8">
-        <div className="col_1 w-[26%] border-1">
+        <div className="col_1 w-[26%]">
           <Button className="!text-black gap-3 w-full ">
             <RiMenu2Fill className="text-[18px]" /> Shop By Categories
             <LiaAngleDownSolid className="text-[13px] ml-auto font-bold" />
@@ -20,48 +19,75 @@ const Navigation = () => {
         <div className="col_2 w-[66%] ">
           <ul className="flex items-center gap-3">
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Home</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+                <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Home</Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Fashion</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Fashion </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Eletronics</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Eletronics </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Bags</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Bags </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Footwear</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Footwear </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Groceries</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Groceries </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Beauty</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Beauty </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Wellness</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Wellness </Button>
+              </Link>
             </li>
 
             <li className="list-none">
-              <Link to="/" className="link transition text-[14px] font-[500]">Jewellery</Link>
+              <Link to="/" className="link transition text-[14px] font-[500]">
+              <Button className="link transition !font-[500] !text-[rgba(0,0,0,0.8)]
+                hover:!text-[#ff5252]"> Jewellery </Button> 
+              </Link>
             </li>
-
-            
-
           </ul>
         </div>
 
         <div className="col_3 w-[20%] flex items-end">
-          <p className="text-[13px] font-[500] flex items-center gap-3 mb-0 mt-0 "> <GoRocket className="text-[18px]" /> Free International Delivery</p>
+          <p className="text-[13px] font-[500] flex items-center gap-3 mb-0 mt-0 ">
+            {" "}
+            <GoRocket className="text-[18px]" /> Free International Delivery
+          </p>
         </div>
       </div>
     </nav>
