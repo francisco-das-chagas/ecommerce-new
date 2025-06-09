7eb000748f8e6f08551c1f6bfc6d9dc8ba396ae3
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-06-09 02:02:03 -0300

    construindo sidebar

--- src/components/Header/Navigation/CategoryPanel.jsx
+++ src/components/Header/Navigation/CategoryPanel.jsx
@@ -1,12 +1,9 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import { IoMdClose } from "react-icons/io";
+
 
 const CategoryPanel = (props) => { 
     const toggleDrawer = (newOpen) => () => {   
@@ -14,30 +11,19 @@ const CategoryPanel = (props) => {
     };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    <Box sx={{ width: 250 }} role="presentation">
 
-      <h3 className='p-3 text-[16px] font-[500]'>Shop By Categories</h3>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-      
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <h3 className='p-3 text-[16px] font-[500] flex items-center justify-between'>Shop By Categories<IoMdClose onClick={toggleDrawer(false)} className='cursor-pointer' /></h3>
+
+      <div className='scroll'>
+        <ul className='w-full'>
+          <li className='list-none'>
+            <Button className='w-full !text-left justify-start'>Fashion</Button>
+          </li>
+        </ul>
+      </div>
+
+ 
     </Box>
   );
 
